b66dd6fcef85d2a5b742dc531d7116318f270dc2
Kelly <kellycyt>
2026-06-13 16:00:12 -0400

    Initial Commit

---
+++ CalorieSnap/CalorieSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieSnapApp.swift
+//  CalorieSnap
+//
+//  Created by Kelly on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CalorieSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
